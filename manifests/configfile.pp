91dfcbb3e808f6fe1c326df92092abb9b557f3eb
Simon Josi <simon.josi@atizo.com>
2012-09-12 18:16:10 +0200

    initial commit

---
+++ manifests/configfile.pp
@@ -0,0 +1,11 @@
+define configfile() {
+  file{"$solr::home/conf/$name":
+    source => [
+      "puppet://$server/modules/site-solr/$name",
+      "puppet://$server/modules/solr/$name",
+    ],
+    require => Class['solr'],
+    notify => Service['tomcat'],
+    owner => $solr::owner, group => $solr::group, mode => 0755;
+  }
+}
---
+++ manifests/init.pp
@@ -0,0 +1,61 @@
+#
+# solr module
+#
+# Copyright 2012, Atizo AG
+# Simon Josi simon.josi+puppet(at)atizo.com
+#
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
+
+class solr(
+  $version = '3.6.1',
+  $home = '/var/lib/solr'
+  $mirror = 'http://mirror.switch.ch/mirror/apache/dist/lucene/solr/',
+  $owner = 'tomcat',
+  $group = 'tomcat',
+  $properties = {}
+) {
+  require tomcat
+
+  $war_source = "$solr::mirror/$solr::version/apache-solr-${solr::version}.tgz"
+  $war_target = "$solr::home/dist/apache-solr-${solr::version}.tgz"
+
+  file{[
+    "$solr::home",
+    "$solr::home/dist",
+    "$solr::home/conf",
+    "$solr::home/data",
+  ]:
+    ensure => directory,
+    owner => $solr::owner, group => $solr::group, mode => 0755;
+  }
+  exec{'fetch_solr_war':
+    command => "wget -O $war_target $war_source",
+    creates => $war_target,
+    require => File["$solr::home/dist"],
+    notify => Service['tomcat'],
+    user => $solr::owner,
+    group => $solr::group,
+    owner => $solr::owner, group => $solr::group, mode => 0644;
+  }
+  file{"/etc/tomcat${lsbmajdistrelease}/Catalaina/localhost/solr.xml":
+    content => template('solr/solr.xml.erb'),
+    notify => Service['tomcat'],
+    owner => root, group => root, mode => 0644;
+  }
+  file{"$solr::home/conf/solrcore.properties":
+    content => template('solrcore.properties'),
+    require => File["$solr::home/conf"],
+    notify => Service['tomcat'],
+    owner => $solr::owner, group => $solr::group, mode => 0755;
+  }
+  solr::configfile{[
+    'scripts.conf',
+    'schema.xml',
+    'solrconfig.xml',
+  ]:}
+}
