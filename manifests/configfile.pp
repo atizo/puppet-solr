8e137624c991d92705b7ec11d398bd3fb6805b77
Simon Josi <simon.josi@atizo.com>
2012-09-12 19:00:57 +0200

    fix dependency cycles

--- manifests/configfile.pp
+++ manifests/configfile.pp
@@ -4,7 +4,6 @@ define solr::configfile() {
       "puppet://$server/modules/site-solr/$name",
       "puppet://$server/modules/solr/$name",
     ],
-    require => Class['solr'],
     notify => Service['tomcat'],
     owner => $solr::owner, group => $solr::group, mode => 0755;
   }
